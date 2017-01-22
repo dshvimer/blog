9539acc38efc2b4c7e7790e9e93a4a30bad10706
David Shvimer <dshvimer@gmail.com>
2017-01-22 14:31:18 -0500

    Handle 404 and precompile tmpl

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ type Post struct {
 	Body  template.HTML
 }
 
+var t = template.New("views")
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("./tmpl/shared/head.tmpl", "./tmpl/home.tmpl"))
 	t.ExecuteTemplate(w, "home", nil)
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
-	t := template.Must(template.ParseFiles("./tmpl/shared/head.tmpl", "./tmpl/404.tmpl"))
 	t.ExecuteTemplate(w, "404", nil)
 }
 
@@ -29,18 +29,24 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 
 	input, err := ioutil.ReadFile("./posts/" + post.Title + ".md")
 	if err != nil {
-		http.Redirect(w, r, "/404", http.StatusNotFound)
+		NotFoundHandler(w, r)
 		return
 	}
 
 	output := blackfriday.MarkdownCommon(input)
 	post.Body = template.HTML(output)
 
-	t := template.Must(template.ParseFiles("./tmpl/shared/head.tmpl", "./tmpl/post.tmpl"))
 	t.ExecuteTemplate(w, "post", post)
 }
 
+func LoadViews() {
+	t = template.Must(t.ParseGlob("./tmpl/*.tmpl"))
+	t = template.Must(t.ParseGlob("./tmpl/shared/*.tmpl"))
+}
+
 func main() {
+	LoadViews()
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.HandleFunc("/", HomeHandler)
